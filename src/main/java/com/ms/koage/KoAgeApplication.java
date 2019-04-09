63a1368b92c517e3baf3d22910a609939015ec15
Daniel Botka <daniel.botka@incepteam.com>
2019-04-09 18:04:41 +0300

    first commit

---
+++ src/main/java/com/ms/koage/KoAgeApplication.java
@@ -0,0 +1,13 @@
+package com.ms.koage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoAgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoAgeApplication.class, args);
+	}
+
+}
